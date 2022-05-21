746c01d1234b726390b8e9358ee35444f0d2badb
ooyuo <doyounjeong0@gmail.com>
2022-05-21 17:26:33 +0900

    style header

--- nomflex-clone/src/App.tsx
+++ nomflex-clone/src/App.tsx
@@ -1,5 +1,20 @@
+import {BrowserRouter as Switch, Router, Routes, Route, BrowserRouter} from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/netflex-clone" element={<Home />} />
+          <Route path="/netflex-clone/tv" element={<Tv />} />
+          <Route path="/netflex-clone/search" element={<Search />} />
+        </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
